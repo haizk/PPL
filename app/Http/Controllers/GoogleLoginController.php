cf6c0e458f36b61b2c196576775ac7e21352b85d
haizk <haizkb@gmail.com>
2024-04-09 17:17:34 +0700

    feat: Prevent login by manually typing path

--- app/Http/Controllers/GoogleLoginController.php
+++ app/Http/Controllers/GoogleLoginController.php
@@ -12,19 +12,30 @@ class GoogleLoginController extends Controller
 {
     public function redirectToGoogle()
     {
-        return Socialite::driver('google')->redirect();
+        try {
+            if (Auth::check()) {
+                return redirect()->route('dashboard');
+            }
+            return Socialite::driver('google')->redirect();
+        } catch (\Exception $e) {
+            return redirect()->route('login');
+        }
     }
 
 
     public function handleGoogleCallback()
     {
-        $googleUser = Socialite::driver('google')->stateless()->user();
-        $user = User::where('email', $googleUser->email)->first();
-        if (!$user) {
-            $user = User::create(['name' => $googleUser->name, 'email' => $googleUser->email, 'password' => Hash::make(bin2hex(random_bytes(16)))]);
-        }
+        try {
+            $googleUser = Socialite::driver('google')->stateless()->user();
+            $user = User::where('email', $googleUser->email)->first();
+            if (!$user) {
+                $user = User::create(['name' => $googleUser->name, 'email' => $googleUser->email, 'password' => Hash::make(bin2hex(random_bytes(16)))]);
+            }
 
-        Auth::login($user);
-        return redirect()->route('dashboard');
+            Auth::login($user);
+            return redirect()->route('dashboard');
+        } catch (\Exception $e) {
+            return redirect()->route('login');
+        }
     }
 }
