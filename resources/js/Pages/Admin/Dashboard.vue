11a3a5bd0cfa63687caea3bbcbcca9590e36eed7
haizk <haizkb@gmail.com>
2024-04-18 23:42:17 +0700

    duar

--- resources/js/Pages/Admin/Dashboard.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import { usePage } from "@inertiajs/vue3";
-
-const page = usePage();
-console.log(page.props);
-</script>
-
-<template>Hello Admin!</template>
--- resources/js/Pages/Admin/Users.vue
+++
@@ -1,1 +0,0 @@
-<template>Hello Admin! Users</template>
