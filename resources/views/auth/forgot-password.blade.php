0a874ea48f4fd9db4d4ae86daa85f2acfa0ebd97
Humanioraaa <muhhilmynaufal03@gmail.com>
2024-04-19 01:01:00 +0700

    first commit

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,45 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+@extends('layouts.auth')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-5 col-lg-6 col-md-7 mx-auto">
+                <div class="reset-passowrd">
+                    <div class="card radius-10 w-100 mt-8">
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <h4>Reset password</h4>
+                                <p>
+                                    {{ __('Forgot your password? No problem. Just let us know
+                                                          your email address and we will email you a password reset
+                                                          link that will allow you to choose a new one.') }}
+                                </p>
+                            </div>
+                            @session('status')
+                                <div class="mb-4 font-medium text-sm text-green-600">
+                                    {{ $value }}
+                                </div>
+                            @endsession
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                            <x-validation-errors class="mb-4" />
+                            <form class="form-body row g-3" method="POST" action="{{ route('password.email') }}">
+                                @csrf
+                                <div class="col-12">
+                                    <label for="inputEmail" class="form-label">Email</label>
+                                    <input type="email" class="form-control" id="email" name="email"
+                                        :value="old('email')" required autofocus autocomplete="username" />
+                                </div>
+                                <div class="col-12 col-lg-12">
+                                    <div class="d-grid">
+                                        <button type="Submit" class="btn btn-primary">
+                                            Send
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        @session('status')
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ $value }}
-            </div>
-        @endsession
-
-        <x-validation-errors class="mb-4" />
-
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <div class="block">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+    </div>
+@endsection
