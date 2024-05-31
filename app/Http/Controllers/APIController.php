e79a5936eea6fa3627ead4891874bbe6ec6a9c10
haizk <haizkb@gmail.com>
2024-05-31 20:46:40 +0700

    feat: Fix APIController, when try to upload vid

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -11,6 +11,8 @@ class APIController extends Controller
     public function upload(Request $request)
     {
         $user = auth()->user();
+        $video = null;
+        $comments = null;
         try {
             $request->validate([
                 'file' => 'required|file|mimetypes:video/mp4,video/quicktime,video/webm,video/x-matroska',
@@ -33,12 +35,11 @@ class APIController extends Controller
 
             $user = $video->user;
             $user_name = $user->name;
-
+            $comments = Comment::where('video_id', $video->id)->get();
         } catch (\Exception $e) {
             $response = $e->getMessage();
         }
 
-        $comments = Comment::where('video_id', $video->id)->get();
 
         //return view('dashboard.details', ['result' => $response, 'video'=>$video]);
         return view('dashboard.details', [
